49b1f56f407082582c3abe67c7d5458ca2c8f508
akchurinaann <anyushka97@bk.ru>
2025-04-20 18:12:21 +0500

    Add files via upload
    Signed-off-by: akchurinaann <anyushka97@bk.ru>

---
+++ pipeline_2.py
@@ -0,0 +1,14 @@
+from kedro.pipeline import Pipeline, node
+from va_akchurina.pipelines.data_science.nodes import select_features_and_train
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+    func=select_features_and_train,
+    inputs=["preprocessed_train", "y_train", "params:paramet"],
+    outputs=["final_model", "model_metrics", "selected_features"],
+    name="train_model_node"
+),
+        ]
+    )
